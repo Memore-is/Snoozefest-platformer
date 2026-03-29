62c94ba09b45515240420794443af620ee360d62
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2026-03-29 19:02:55 -0400

    added background

--- Snoozefest/scripts/player.gd
+++ Snoozefest/scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 900.0
+var SPEED = 500.0
 const JUMP_VELOCITY = -900.0
 
 
@@ -9,7 +9,9 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		
+	if global_position.y > 200:
+		get_tree().reload_current_scene()
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
